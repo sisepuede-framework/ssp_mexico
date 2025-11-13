f378f118578de8bb01e664f96cd03b4ae571b330
fabian fuentes <cffuentes@tec.mx>
2025-11-13 12:21:20 -0600

    update 251112

--- ssp_modeling/output_postprocessing/postprocessing_mexico.r
+++ ssp_modeling/output_postprocessing/postprocessing_mexico.r
@@ -1,29 +1,29 @@
-    #################################################
-    # Post processing process
-    #################################################
+#################################################
+# Post processing process
+#################################################
 
-    # load packages
-    library(data.table)
-    library(reshape2)
-    library(mFilter)
-    library(ggplot2)
-    library(dplyr)
+# load packages
+library(data.table)
+library(reshape2)
+library(mFilter)
+library(ggplot2)
+library(dplyr)
 
-    rm(list=ls())
+rm(list=ls())
 
-    #ouputfile
+#ouputfile
 
-    dir.output  <- "ssp_modeling/ssp_run_output/sisepuede_results_sisepuede_run_2025-10-23T13;32;38.383522/"
-    output.file <- "sisepuede_results_sisepuede_run_2025-10-23T13;32;38.383522_WIDE_INPUTS_OUTPUTS.csv"
+dir.output  <- "ssp_modeling/ssp_run_output/sisepuede_results_sisepuede_run_2025-11-12T11;28;48.536409/"
+output.file <- "sisepuede_results_sisepuede_run_2025-11-12T11;28;48.536409_WIDE_INPUTS_OUTPUTS.csv"
 
-    region <- "mexico" 
-    iso_code3 <- "MEX"
+region <- "mexico" 
+iso_code3 <- "MEX"
 
-    year_ref <- 2023
+year_ref <- 2023
 
-    source('ssp_modeling/output_postprocessing/scr/run_script_baseline_run_new.r')
+source('ssp_modeling/output_postprocessing/scr/run_script_baseline_run_new.r')
 
-    source('ssp_modeling/output_postprocessing/scr/data_prep_new_mapping.r')
+source('ssp_modeling/output_postprocessing/scr/data_prep_new_mapping.r')
 
 source('ssp_modeling/output_postprocessing/scr/data_prep_drivers.r')
 
