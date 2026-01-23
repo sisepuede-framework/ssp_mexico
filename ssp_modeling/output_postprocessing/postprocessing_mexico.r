8678a35ad2fcbc7d8700a8aca244c70d695d16b2
fabian fuentes <cffuentes@tec.mx>
2026-01-22 22:46:31 -0600

    last ver 220126

--- ssp_modeling/output_postprocessing/postprocessing_mexico.r
+++ ssp_modeling/output_postprocessing/postprocessing_mexico.r
@@ -13,8 +13,7 @@ rm(list=ls())
 
 #ouputfile
 
-#run <- 'sisepuede_results_sisepuede_run_2026-01-21T19;20;33.022925'
-run <- 'sisepuede_results_sisepuede_run_2026-01-22T18;02;33.694818'
+run <- 'sisepuede_results_sisepuede_run_2026-01-22T22;03;00.238547'
 
 dir.output  <- paste0("ssp_modeling/ssp_run_output/",run,"/")
 output.file <- paste0(run, "_WIDE_INPUTS_OUTPUTS.csv")
