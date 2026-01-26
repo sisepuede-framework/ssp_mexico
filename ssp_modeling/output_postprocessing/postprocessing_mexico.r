6a938051a4b2e363120dbc58a6749ce70b3d66cd
fabian fuentes <cffuentes@tec.mx>
2026-01-26 13:34:19 -0600

    update natural gas prices

--- ssp_modeling/output_postprocessing/postprocessing_mexico.r
+++ ssp_modeling/output_postprocessing/postprocessing_mexico.r
@@ -13,7 +13,7 @@ rm(list=ls())
 
 #ouputfile
 
-run <- 'sisepuede_results_sisepuede_run_2026-01-26T12;12;06.123928'
+run <- 'sisepuede_results_sisepuede_run_2026-01-26T12;59;15.354017'
 
 dir.output  <- paste0("ssp_modeling/ssp_run_output/",run,"/")
 output.file <- paste0(run, "_WIDE_INPUTS_OUTPUTS.csv")
